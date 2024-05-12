c4cf0073c8caae393cc0040197197f1831aa42d1
Acrasking <93838235+acresking@users.noreply.github.com>
2024-05-12 22:06:02 +0300

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,34 @@
+import requests
+import socket
+
+API_KEY = '832b95377a624c9480429dba66575e9a'
+
+def fetch_domain_info(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        url = f'https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip}'
+        response = requests.get(url)
+        data = response.json()
+
+        if 'error' in data:
+            print(f'Error: {data["error"]["message"]}')
+        else:
+            display_domain_info(data)
+    except socket.gaierror:
+        print('Error: Failed to resolve domain.')
+
+def display_domain_info(data):
+    print('מידע מהדוח:')
+    for key, value in data.items():
+        if isinstance(value, dict):
+            print(f'{key}:')
+            for inner_key, inner_value in value.items():
+                print(f'  {inner_key}: {inner_value}')
+        else:
+            print(f'{key}: {value}')
+
+domain = input('אנא הכנס את שם הדומיין שברצונך לבדוק: ')
+if domain:
+    fetch_domain_info(domain)
+else:
+    print('לא הוזן דומיין, פעולה בוטלה.')
