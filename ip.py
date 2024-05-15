71f1aa58cf420db521079b98f764fe577a94b426
Acrasking <93838235+acresking@users.noreply.github.com>
2024-05-15 14:57:44 +0300

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,30 +1,37 @@
+from bs4 import BeautifulSoup
 import requests
 import socket
-
-API_KEY = 'ה-API_KEY_שלך_כאן'
 
 def fetch_domain_info(domain):
     try:
         ip = socket.gethostbyname(domain)
-        url = f'https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip}'
+        url = f'https://ipgeolocation.io/what-is-my-ip/{ip}'
         response = requests.get(url)
-        data = response.json()
 
-        if 'error' in data:
-            print(f'Error: {data["error"]["message"]}')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            data_table = soup.find('table', {'class': 'table'})
+            if data_table:
+                display_domain_info(data_table)
+            else:
+                print('Error: Data table not found in HTML.')
+
         else:
-            display_domain_info(data)
+            print(f'Error: Non-200 status code ({response.status_code})')
+
     except socket.gaierror:
         print('Error: Failed to resolve domain.')
+    except Exception as e:
+        print(f'Error: {e}')
 
-def display_domain_info(data):
-    print('מידע מהדוח:')
-    for key, value in data.items():
-        if isinstance(value, dict):
-            print(f'{key}:')
-            for inner_key, inner_value in value.items():
-                print(f'  {inner_key}: {inner_value}')
-        else:
+def display_domain_info(data_table):
+    print('מידע מהדומיין:')
+    rows = data_table.find_all('tr')
+    for row in rows:
+        columns = row.find_all('td')
+        if len(columns) == 2:
+            key = columns[0].text.strip()
+            value = columns[1].text.strip()
             print(f'{key}: {value}')
 
 domain = input('אנא הכנס את שם הדומיין שברצונך לבדוק: ')
